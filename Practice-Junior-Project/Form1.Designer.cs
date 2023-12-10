7ad3bd2a1bada80984534765ced3c7298f102913
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-11 05:47:45 +0700

    feat: create Login form

--- Practice-Junior-Project/Form1.Designer.cs
+++ Practice-Junior-Project/Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            tbUsername = new TextBox();
+            tbPassword = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Login";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 1;
+            label2.Text = "username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 78);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 2;
+            label3.Text = "password";
+            // 
+            // tbUsername
+            // 
+            tbUsername.Location = new Point(76, 41);
+            tbUsername.Name = "tbUsername";
+            tbUsername.Size = new Size(100, 23);
+            tbUsername.TabIndex = 3;
+            // 
+            // tbPassword
+            // 
+            tbPassword.Location = new Point(76, 75);
+            tbPassword.Name = "tbPassword";
+            tbPassword.Size = new Size(100, 23);
+            tbPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(12, 122);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btnLogin_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLogin);
+            Controls.Add(tbPassword);
+            Controls.Add(tbUsername);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tbUsername;
+        private TextBox tbPassword;
+        private Button btnLogin;
     }
 }
--- Practice-Junior-Project/Form1.cs
+++ Practice-Junior-Project/Form1.cs
@@ -6,5 +6,29 @@ namespace Practice_Junior_Project
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login()
+        {
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+            if (username == "admin" && password == "admin")
+            {
+                MessageBox.Show("Login successful");
+            }
+            else
+            {
+                MessageBox.Show("Login failed");
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            Login();
+        }
     }
 }
