4173615352321501c1c3d4d8458a80e8e4a6d044
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-11 05:28:30 +0700

    feat: initial commit

---
+++ Practice-Junior-Project/Form1.cs
@@ -0,0 +1,10 @@
+namespace Practice_Junior_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Practice-Junior-Project
+ 
