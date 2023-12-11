6b5e6617c858b138328b13134de86c68e8b87735
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-11 07:25:14 +0700

    feat: complete LoginUser logic

--- Practice-Junior-Project/Form1.cs
+++ Practice-Junior-Project/Form1.cs
@@ -18,23 +18,56 @@ namespace Practice_Junior_Project
 
         }
 
-        private void Login()
+        private void LoginUser()
         {
             string username = tbUsername.Text;
             string password = tbPassword.Text;
-            if (username == "admin" && password == "admin")
-            {
-                MessageBox.Show("Login successful");
-            }
-            else
-            {
+            
+            conn = new NpgsqlConnection(connString);
 
+            if(username == "" || password == "")
+            {
+                MessageBox.Show("Username or password cannot be empty", "Error");
+                return;
+            }   
+
+            try
+            {
+                conn.Open();
+                string sql = "SELECT login_user(@in_username, @in_password) AS http_status_code;";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@in_username", username);
+                cmd.Parameters.AddWithValue("@in_password", password);
+
+                int httpStatusCode = (int)cmd.ExecuteScalar();
+
+                if(httpStatusCode == 200)
+                {
+                    MessageBox.Show("Login successful");
+                }
+                else if(httpStatusCode == 401)
+                {
+                    throw new Exception("Username or password is incorrect");
+                }
+                else if(httpStatusCode == 500)
+                {
+                    throw new Exception("Internal server error");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
             }
         }
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            Login();
+            LoginUser();
         }
 
         private void btnToRegister_Click(object sender, EventArgs e)
