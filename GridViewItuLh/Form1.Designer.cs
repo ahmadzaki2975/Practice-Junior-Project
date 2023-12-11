d032fee66c02db6ba09f79ce598b95522f8794b5
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2023-12-11 11:21:50 +0700

    feat: done Load and Insert data

--- GridViewItuLh/Form1.Designer.cs
+++ GridViewItuLh/Form1.Designer.cs
@@ -28,12 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            tbUsername = new TextBox();
+            tbPassword = new TextBox();
+            btnInsert = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnLoadData = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Data User";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 55);
+            label2.Name = "label2";
+            label2.Size = new Size(59, 15);
+            label2.TabIndex = 1;
+            label2.Text = "username";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 85);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 15);
+            label3.TabIndex = 2;
+            label3.Text = "password";
+            // 
+            // tbUsername
+            // 
+            tbUsername.Location = new Point(77, 52);
+            tbUsername.Name = "tbUsername";
+            tbUsername.Size = new Size(219, 23);
+            tbUsername.TabIndex = 3;
+            // 
+            // tbPassword
+            // 
+            tbPassword.Location = new Point(77, 82);
+            tbPassword.Name = "tbPassword";
+            tbPassword.Size = new Size(219, 23);
+            tbPassword.TabIndex = 4;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(336, 52);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(96, 53);
+            btnInsert.TabIndex = 5;
+            btnInsert.Text = "Insert";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(438, 52);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(96, 53);
+            btnUpdate.TabIndex = 6;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(540, 52);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(96, 53);
+            btnDelete.TabIndex = 7;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnLoadData
+            // 
+            btnLoadData.Location = new Point(134, 9);
+            btnLoadData.Name = "btnLoadData";
+            btnLoadData.Size = new Size(90, 30);
+            btnLoadData.TabIndex = 8;
+            btnLoadData.Text = "Load Data";
+            btnLoadData.UseVisualStyleBackColor = true;
+            btnLoadData.Click += btnLoadData_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 136);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(776, 302);
+            dataGridView1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnLoadData);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnInsert);
+            Controls.Add(tbPassword);
+            Controls.Add(tbUsername);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox tbUsername;
+        private TextBox tbPassword;
+        private Button btnInsert;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnLoadData;
+        private DataGridView dataGridView1;
     }
 }
--- GridViewItuLh/Form1.cs
+++ GridViewItuLh/Form1.cs
@@ -1,3 +1,8 @@
+using Npgsql;
+using System.Data;
+using System.Diagnostics;
+using System.Text.Json.Nodes;
+
 namespace GridViewItuLh
 {
     public partial class Form1 : Form
@@ -5,6 +10,78 @@ namespace GridViewItuLh
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string connString = "Host=localhost;Port=5433;Database=postgres;Username=postgres;Password=postgres";
+        NpgsqlConnection conn;
+        public DataTable dt;
+        private DataGridViewRow row;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoadData_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new NpgsqlConnection(connString);
+                conn.Open();
+                string sql = "SELECT * FROM users;";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+                dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string username = tbPassword.Text;
+            string password = tbPassword.Text;
+
+            try
+            {
+                conn = new NpgsqlConnection(connString);
+                conn.Open();
+                string sql = "SELECT * FROM register_user(@username, @password);";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, conn);
+
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@password", password);
+
+                int statusCode = (int)cmd.ExecuteScalar();
+
+                if (statusCode == 201)
+                {
+                    MessageBox.Show("Insert user success");
+                }
+                else if (statusCode == 401)
+                {
+                    throw new Exception("Username already exist");
+                }
+                else
+                {
+                    throw new Exception("An error occured");
+                }
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
